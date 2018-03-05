38722d12069cf17a2b83a5a87a32b41ef6f9f53f
Alex McCool <alex.mccool@yahoo.com>
2018-03-05 17:32:18 +0200

    doubled up

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RB.FacilityConfig.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -7,6 +11,33 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var x = Task.Run(async () =>
+            {
+                var facilities = await GetFacilityList();
+
+                foreach(var facility in facilities)
+                {
+                    Console.WriteLine(facility);
+                }
+
+            });
+
+
+            x.Wait();
+
+            Console.ReadLine();
+
         }
+
+        static public async Task<List<string>> GetFacilityList()
+        {
+            IFacilityConfigurationClient fc = new FacilityConfigurationClient(new Uri("http://facilityconfigurationservice.devint.dev-r5ead.net"));
+
+            var facilities = await fc.GetAllFacilities();
+
+            return facilities.Select(a=>a.FacilityName).ToList();
+        }
+
     }
 }
